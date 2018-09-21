26920bf9f25cd54a095757cc1489f1d70fa0e49a
21克的爱情 <raohong07@163.com>
2018-09-21 11:56:08 +0800

    新增demo

---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+    <img v-initi="{ data: src, config: {width: '100px',height: '100px',borderRadius: '50%'}}">
+    <div class="demo">
+      <div v-initi="{ data: content1, config: {height: '24px',overflow: 'hidden', textOverflow: 'ellipsis'}}"></div>
+      <div v-initi="{ data: content2, config: {height: '24px',overflow: 'hidden', textOverflow: 'ellipsis'}}"></div>
+      <div v-initi="{ data: content3, config: {height: '24px',overflow: 'hidden', textOverflow: 'ellipsis'}}"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      content1: '',
+      content2: '',
+      content3: '',
+      src: ''
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.content1 = 'Hello world！'
+      this.content3 = 'Hello world！'
+      this.src = 'https://cn.vuejs.org/images/logo.png'
+    }, 1000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+.demo{
+  display:inline-block;
+  width:500px
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
